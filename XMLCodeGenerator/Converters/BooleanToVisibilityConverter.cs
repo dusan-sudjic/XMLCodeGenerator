41d09c78b95a30297ba9130a70ccefba975c27cc
dusan-sudjic <duci.sudjic@gmail.com>
2024-06-20 14:01:34 +0200

    importing providers

---
+++ XMLCodeGenerator/Converters/BooleanToVisibilityConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows;
+
+namespace XMLCodeGenerator.Converters
+{
+    public class BooleanToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool && (bool)value)
+                return Visibility.Visible;
+            return Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is Visibility && (Visibility)value == Visibility.Collapsed)
+                return false;
+            return true;
+        }
+    }
+}
--- XMLCodeGenerator/MainWindow.xaml.cs
+++ XMLCodeGenerator/MainWindow.xaml.cs
@@ -1,7 +1,10 @@
-﻿using System.Data.Common;
+﻿using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Data.Common;
 using System.IO;
 using System.Reflection;
 using System.Reflection.Metadata;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Windows;
@@ -15,6 +18,7 @@ using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Xml;
 using XMLCodeGenerator.Commands;
 using XMLCodeGenerator.Model;
 using XMLCodeGenerator.Model.BuildingBlocks;
@@ -28,9 +32,34 @@ using static System.Net.Mime.MediaTypeNames;
 
 namespace XMLCodeGenerator
 {
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
-        public List<ClassInfo> Classes = new();
+        public List<ClassInfo> ProviderReaderClasses = new();
+        public List<EntityInfo> SourceProviderEntities = new();
+        private bool _isProviderReaderImported;
+        public bool IsProviderReaderImported { 
+            get => _isProviderReaderImported; 
+            set { 
+                if(value!=_isProviderReaderImported) 
+                { 
+                    _isProviderReaderImported = value;
+                    OnPropertyChanged();
+                }
+            } 
+        }
+        private bool _isSourceProviderImported;
+        public bool isSourceProviderImported
+        {
+            get => _isSourceProviderImported;
+            set
+            {
+                if (value != _isSourceProviderImported)
+                {
+                    _isSourceProviderImported = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
         public ICommand AddClassCommand { get; set; }
         public static List<ICim> CimClasses { get; set; }
         public static StackPanel CimClassesStackPanel { get; set; }
@@ -40,6 +69,15 @@ namespace XMLCodeGenerator
             InitializeComponent();
             CimClasses = new List<ICim>();
             AddClassCommand = new RelayCommand(ExecuteAddNewCimClassCommand);
+            this.DataContext = this;
+            CimClassesStackPanel = (StackPanel)this.FindName("Stack");
+            XMLPreviewTextBlock = (TextBlock)this.FindName("XMLPreview");
+            IsProviderReaderImported = false;
+            MockData();
+        }
+
+        private void MockData()
+        {
             var class1 = new CimClassElement();
             class1.Attributes.Where(a => a.Name == "name").ToList()[0].Value = "ACLineSegment";
             var property1 = new CimPropertyElement();
@@ -68,19 +106,7 @@ namespace XMLCodeGenerator
             if1.AddChildElementToContent(abs);
             property2.AddChildElementToContent(new GetValueElement());
             property3.AddChildElementToContent(new ConstantElement());
-            //List<string> classes = ElementProvider.GetSupportedChildElements(RootElement);  //getting options
-            //var instance = ElementProvider.CreateNewElement(classes[5]);  //choosing an option
-            //RootElement.AddChildElementToContent(instance);
-            //List<string> classes2 = ElementProvider.GetSupportedChildElements(instance);
-            //var instance2 = ElementProvider.CreateNewElement(classes2[2]);
-            //instance.AddChildElementToContent(instance2);
-            //instance2.AddChildElementToContent(ElementProvider.CreateNewElement(classes2[0]));
-            ////instance2.AddChildElementToContent(ElementProvider.CreateNewElement(classes2[0]));
-            //instance.AddChildElementToContent(ElementProvider.CreateNewElement(classes2[0]));
-            this.DataContext = this;
-
-            CimClassesStackPanel = (StackPanel)this.FindName("Stack");
-            XMLPreviewTextBlock = (TextBlock)this.FindName("XMLPreview");
+            
             CimClasses.Add(class1);
             CimClassesStackPanel.Children.Add(new ElementUserControl(class1));
             CimClasses.Add(class1);
@@ -90,6 +116,7 @@ namespace XMLCodeGenerator
             CimClasses.Add(class1);
             CimClassesStackPanel.Children.Add(new ElementUserControl(class1));
         }
+
         public static void BindElementToXMLPreview(IElement element)
         {
             XMLPreviewTextBlock.Inlines.Clear();
@@ -105,17 +132,11 @@ namespace XMLCodeGenerator
             foreach (Match match in matches)
             {
                 if (match.Groups["boldText"].Success)
-                {
                     inlines.Add(new Run(match.Groups["boldText"].Value) { FontWeight = FontWeights.Bold });
-                }
                 else if (match.Groups["redText"].Success)
-                {
                     inlines.Add(new Run(match.Groups["redText"].Value) { Foreground = Brushes.DarkOrange, FontWeight=FontWeights.DemiBold });
-                }
                 else if (match.Groups["normalText"].Success)
-                {
                     inlines.Add(new Run(match.Groups["normalText"].Value));
-                }
             }
             return inlines.ToArray();
         }
@@ -146,8 +167,9 @@ namespace XMLCodeGenerator
         {
             AddNewCimClass();
         }
-        public void ReadClassesFromFileClick(object sender, RoutedEventArgs e)
+        public void ImportProviderReader(object sender, RoutedEventArgs e)
         {
+            ProviderReaderClasses.Clear();
             Microsoft.Win32.OpenFileDialog openFileDialog = new Microsoft.Win32.OpenFileDialog();
             openFileDialog.Filter = "DLL Files (*.dll)|*.dll";
 
@@ -163,14 +185,49 @@ namespace XMLCodeGenerator
                     {
                         string className = type.FullName;
                         className += type.BaseType.Name!="Object" ? $" (inherits from {type.BaseType.Name})":"";
-                        string properties = string.Join("", GetClassProperties(type));
-                        Classes.Add(new ClassInfo { ClassName = className, Properties = properties });
+                        ProviderReaderClasses.Add(new ClassInfo { ClassName = className, Properties = GetClassProperties(type) });
                     }
-                    //classListBox.ItemsSource = Classes;
+                    IsProviderReaderImported = true;
                 }
                 catch (Exception ex)
                 {
                     MessageBox.Show($"Error loading classes from file: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+        }
+        public void ImportSourceProvider(object sender, RoutedEventArgs e)
+        {
+            SourceProviderEntities.Clear();
+            Microsoft.Win32.OpenFileDialog openFileDialog = new Microsoft.Win32.OpenFileDialog();
+            openFileDialog.Filter = "XML Files (*.xml)|*.xml";
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                string filePath = openFileDialog.FileName;
+                try
+                {
+                    XmlDocument xmlDoc = new XmlDocument();
+                    xmlDoc.Load(filePath);
+                    XmlNodeList entitityNodes = xmlDoc.SelectNodes("//Entity");
+
+                    if (entitityNodes != null)
+                    {
+                        foreach (XmlNode personNode in entitityNodes)
+                        {
+                            string name = personNode.SelectSingleNode("Name")?.InnerText;
+                            EntityInfo entity = new EntityInfo { Name = name.Trim(), Attributes = new() };
+                            foreach (XmlNode attributeNode in personNode.SelectNodes("EntityAttribute"))
+                                entity.Attributes.Add(attributeNode.Attributes["Name"]?.Value);
+                            SourceProviderEntities.Add(entity);
+                        }
+                        isSourceProviderImported = true;
+                    }
+                    else
+                        MessageBox.Show("No <Entity> elements found in the XML document.");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Error loading or processing XML file: {ex.Message}");
                 }
             }
         }
@@ -181,7 +238,17 @@ namespace XMLCodeGenerator
                 properties.Add($".\t{property.Name}: {property.PropertyType.Name}\n");
             return properties;
         }
-        
+        public void ResizeThumb_DragDelta(object sender, DragDeltaEventArgs e)
+        {
+            var thumb = (Thumb)sender;
+            var border = (Border)thumb.Parent;
+
+            double deltaX = e.HorizontalChange;
+            double deltaY = e.VerticalChange;
+
+            border.Width = Math.Max(border.Width + deltaX, thumb.DesiredSize.Width);
+            border.Height = Math.Max(border.Height + deltaY, thumb.DesiredSize.Height);
+        }
         private async void ScrollToBottomSmoothlyAsync()
         {
             ScrollViewer scrollViewer = (ScrollViewer)this.FindName("CimClassesScroll");
@@ -201,42 +268,29 @@ namespace XMLCodeGenerator
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
             MaximizeToWorkingArea();
-            //var workingArea = SystemParameters.WorkArea;
-
-            //this.Width = workingArea.Width;
-            //this.Height = workingArea.Height;
-
-            //this.Left = workingArea.Left;
-            //this.Top = workingArea.Top;
         }
 
         private void MaximizeToWorkingArea()
         {
             var workingArea = SystemParameters.WorkArea;
 
-            this.WindowState = WindowState.Normal; // Ensure the window is in normal state first
-            this.WindowStyle = WindowStyle.SingleBorderWindow; // Ensure the window style is correct
-            this.ResizeMode = ResizeMode.CanResize; // Allow the window to be resized, to retain the border
+            this.WindowState = WindowState.Normal; 
+            this.WindowStyle = WindowStyle.SingleBorderWindow; 
+            this.ResizeMode = ResizeMode.CanResize; 
 
             this.Width = workingArea.Width;
             this.Height = workingArea.Height;
             this.Left = workingArea.Left;
             this.Top = workingArea.Top;
 
-            // Ensure the window is brought to the foreground
-            this.Topmost = true; // Optional: keep the window on top of other windows
-            this.Topmost = false; // Reset Topmost to allow interaction with other windows
+            this.Topmost = true; 
+            this.Topmost = false;
         }
-        public void ResizeThumb_DragDelta(object sender, DragDeltaEventArgs e)
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
         {
-            var thumb = (Thumb)sender;
-            var border = (Border)thumb.Parent;
-
-            double deltaX = e.HorizontalChange;
-            double deltaY = e.VerticalChange;
-
-            border.Width = Math.Max(border.Width + deltaX, thumb.DesiredSize.Width);
-            border.Height = Math.Max(border.Height + deltaY, thumb.DesiredSize.Height);
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
 
     }
@@ -244,6 +298,11 @@ namespace XMLCodeGenerator
     public record ClassInfo
     {
         public string ClassName { get; set; }
-        public string Properties { get; set; }
+        public List<string> Properties { get; set; }
+    }
+    public record EntityInfo
+    {
+        public string Name { get; set; }
+        public List<string> Attributes { get; set; }
     }
 }
