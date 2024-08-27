e508fbae96b1bf087580fc88331b520db59b2bcc
dusan-sudjic <duci.sudjic@gmail.com>
2024-08-27 11:30:12 +0200

    feat: mapping attributes from source provider entities to GetValue element

--- XMLCodeGenerator/View/ProviderWindow.xaml.cs
+++ XMLCodeGenerator/View/ProviderWindow.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -19,10 +21,22 @@ using XMLCodeGenerator.ViewModel;
 
 namespace XMLCodeGenerator.View
 {
-    public partial class ProviderWindow : Window
+    public partial class ProviderWindow : Window, INotifyPropertyChanged
     {
+        private bool tabs;
+        public bool Tabs
+        {
+            get => tabs;
+            set
+            {
+                if (value != tabs)
+                {
+                    tabs = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
         public ListBox ValuesListBox { get; set; }
-        public string SelectedValue { get; set; }
         WatermarkTextBox searchTextBox { get; set; }
         List<ProviderElement> ProviderElements { get; set; }
         public bool MultiSelect { get; set; } = false;
@@ -38,12 +52,14 @@ namespace XMLCodeGenerator.View
             {
                 case Model.Elements.InputType.CIM_PROFILE_CLASS: 
                     {
+                        Tabs = false;
                         foreach (var c in MainWindow.CimProfileClasses)
                             ProviderElements.Add(c);
                         break; 
                     }
                 case Model.Elements.InputType.CIM_PROFILE_PROPERTY: 
                     {
+                        Tabs = false;
                         string className = attribute.Element.Parent.Attributes[0].Value;
                         CimProfileClass cl = MainWindow.CimProfileClasses.Where(c => c.Name == className).FirstOrDefault();
                         if (cl == null)
@@ -59,7 +75,8 @@ namespace XMLCodeGenerator.View
                         break; 
                     }
                 case Model.Elements.InputType.SOURCE_PROVIDER_ENTITY: 
-                    { 
+                    {
+                        Tabs = false;
                         MultiSelect = true;
                         foreach (var c in MainWindow.SourceProviderEntities)
                             ProviderElements.Add(c);
@@ -67,19 +84,37 @@ namespace XMLCodeGenerator.View
                     }
                 case Model.Elements.InputType.SOURCE_PROVIDER_ATTRIBUTE: 
                     {
-                        string entityName = attribute.Element.Parent.Attributes[0].Value;
-                        SourceProviderEntity cl = MainWindow.SourceProviderEntities.Where(c => c.Name == entityName).FirstOrDefault();
-                        foreach (var c in cl.Attributes)
-                            ProviderElements.Add(c);
+                        Tabs = true;
+                        string[] values = FindValuesFromCimClass(attribute.Element);
+                        List<SourceProviderEntity> entities;
+                        if (values != null)
+                            entities = MainWindow.SourceProviderEntities.Where(c => values.Any(v => v.Equals(c.Name))).ToList();
+                        else
+                            entities = MainWindow.SourceProviderEntities;
+                        TabControl tabControl = (TabControl)this.FindName("TabControl");
+                        foreach(var e in entities)
+                        {
+                            ListBox lb = new ListBox();
+                            lb.ItemsSource = e.Attributes;
+                            TabItem item = new TabItem
+                            {
+                                Header = e.Name,
+                                Content = lb
+                            };
+                            tabControl.Items.Add(item);
+                        }
                         break; 
                     }
                 default: break;
             }
-            ValuesListBox.ItemsSource = ProviderElements;
-            ValuesListBox.SelectedIndex = MultiSelect ? -1 : 0;
+            if (!Tabs)
+            {
+                ValuesListBox.ItemsSource = ProviderElements;
+                ValuesListBox.SelectedIndex = MultiSelect ? -1 : 0;
+                selectDefaultValues();
+            }
             searchTextBox = (WatermarkTextBox)this.FindName("search");
             searchTextBox.Focus();
-            selectDefaultValues();
         }
         private void selectDefaultValues()
         {
@@ -157,29 +192,43 @@ namespace XMLCodeGenerator.View
 
         private void Select()
         {
-            if (MultiSelect)
+            if (!Tabs)
             {
-                if (ValuesListBox.SelectedItems.Count == 0)
+                if (MultiSelect)
                 {
-                    System.Windows.MessageBox.Show("Please select values.");
-                    return;
+                    if (ValuesListBox.SelectedItems.Count == 0)
+                    {
+                        System.Windows.MessageBox.Show("Please select values.");
+                        return;
+                    }
+                    string selectedValue = "";
+                    foreach (ProviderElement provider in ValuesListBox.SelectedItems)
+                        selectedValue = selectedValue + provider.Name + (ValuesListBox.SelectedItems.IndexOf(provider) == ValuesListBox.SelectedItems.Count - 1 ? "" : ", ");
+                    Attribute.Value = selectedValue;
+                    this.DialogResult = true;
                 }
-                SelectedValue = "";
-                foreach(ProviderElement provider in ValuesListBox.SelectedItems)
-                    SelectedValue = SelectedValue + provider.Name + (ValuesListBox.SelectedItems.IndexOf(provider)==ValuesListBox.SelectedItems.Count-1 ? "" : ", ");
-                Attribute.Value = SelectedValue;
-                this.DialogResult = true;
-            }
-            else if (ValuesListBox.SelectedItem != null)
-            {
-                var provider = ValuesListBox.SelectedItem as ProviderElement;
-                SelectedValue = provider.Name;
-                Attribute.Value = SelectedValue;
-                this.DialogResult = true;
+                else if (ValuesListBox.SelectedItem != null)
+                {
+                    var provider = ValuesListBox.SelectedItem as ProviderElement;
+                    Attribute.Value = provider.Name;
+                    this.DialogResult = true;
+                }
+                else
+                {
+                    System.Windows.MessageBox.Show("Please select an option.");
+                }
             }
             else
             {
-                System.Windows.MessageBox.Show("Please select an option.");
+                TabControl tabControl = (TabControl)this.FindName("TabControl");
+                var provider = (tabControl.SelectedContent as ListBox).SelectedItem as ProviderElement;
+                if (provider == null)
+                {
+                    System.Windows.MessageBox.Show("Please select an option.");
+                    return;
+                }
+                Attribute.Value = provider.Name;
+                this.DialogResult = true;
             }
         }
 
@@ -193,6 +242,20 @@ namespace XMLCodeGenerator.View
             }
             ValuesListBox.ItemsSource = newList;
             if (ValuesListBox.SelectedIndex == -1) ValuesListBox.SelectedIndex = 0;
+        }
+        private string[] FindValuesFromCimClass(ElementViewModel vm)
+        {
+            if (vm.Parent == null)
+                return null;
+            if (vm.Parent.Name.Equals("CimClass"))
+                return vm.Parent.Attributes[1].Value.Split(',').Select(v=>v.Trim()).ToArray();
+            return FindValuesFromCimClass(vm.Parent);
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
